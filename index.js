3cde2dbbe588e33050d2a0e01292b16d8e4054e5
Ali Ayub Khan <akhan.bscs15seecs@seecs.edu.pk>
2017-05-14 11:36:11 +0500

    check

--- index.js
+++ index.js
@@ -32,11 +32,31 @@ function handleEvent(event) {
     return Promise.resolve(null);
   }
   
-  var ans = "hello";
+  var ans;
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
+app.post('/', function(req, response) {
+	response.writeHead(200);
+	
+  var options1 = { 
+					method: 'GET',
+					url: 'http://api.asksusi.com/susi/chat.json',
+					qs: { timezoneOffset: '-330', q: event.message.text }
+				};
+				
+		// A request to the Susi bot
+		request(options1, function (error1, response1, body1) {
+  			if (error1) throw new Error(error1);
+  			// answer fetched from susi
+			ans = (JSON.parse(body1)).answers[0].actions[0].expression;
+		});
+});
 			
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  //const echo = { type: 'text', text: event.message.text };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
