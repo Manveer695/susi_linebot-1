197d362c7ac6b11d32e6d5eb636c97076e302364
Ali Ayub Khan <akhan.bscs15seecs@seecs.edu.pk>
2017-05-14 13:43:56 +0500

    check

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function handleEvent(event) {
   }
 
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  const echo = { type: 'text', text:'hi' + event.message.text  };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
