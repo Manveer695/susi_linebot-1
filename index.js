a46fc93d8465d6b8e3f1a181e091f18f68bf0f24
Ali Ayub Khan <akhan.bscs15seecs@seecs.edu.pk>
2017-05-14 09:31:56 +0500

    check

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ const app = express();
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/webhook', line.middleware(config), (req, res) => {
-  Promise.all(req.body.events.map(handleEvent))
-  json.then(Promise.reject.bind(Promise));
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
 });
 
 // event handler
